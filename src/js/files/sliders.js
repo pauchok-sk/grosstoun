43879a88e5508e31c90e5d3070bd97f69789133d
pauchok-sk <pauchoooooook@gmail.com>
2025-08-27 00:17:40 +0300

    columns page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -30,4 +30,37 @@ export default function sliders() {
       });
     });
   }
+
+  const heroSlider = document.querySelector(".hero__slider");
+
+  if (heroSlider) {
+    const thumbSlider = document.querySelector(".hero__thumb-slider");
+
+    const thumbSwiper = new Swiper(thumbSlider, {
+      speed: 800,
+      spaceBetween: 20,
+      slidesPerView: "auto",
+      breakpoints: {
+        768: {
+          spaceBetween: 24,
+          slidesPerView: 3,
+        },
+      },
+    });
+
+    const swiper = new Swiper(heroSlider, {
+      speed: 800,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 3500
+      },
+      thumbs: {
+        swiper: thumbSwiper,
+      },
+      navigation: {
+        prevEl: ".hero__slider-arrow._prev",
+        nextEl: ".hero__slider-arrow._next"
+      }
+    });
+  }
 }
